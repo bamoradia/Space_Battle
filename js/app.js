2475c42f7a290882cd00826f8c3480b318fef4c6
Badal Moradia <badal@i360llc.com>
2018-06-20 12:22:32 -0500

    made and tested battle function in single combat

--- js/app.js
+++ js/app.js
@@ -3,13 +3,16 @@ console.log('Javascript is linked')
 
 class AlienShip{
 	constructor(){
+		//use random numbers to generate stats for each instatiation of the class
 		this.hull = randomNumber(3, 6);
 		this.firepower = randomNumber(2, 4)
 		this.accuracy = randomNumber(.6, .8, 'dec')
 	}
+	//reduces the hull strength by the firepower of the opposing ship
 	receiveDamage(damage){
 		this.hull -= damage;
 	}
+	//made an attack method which uses a random number to determine if the attack was succesful or not
 	attack(){
 		if(Math.random() < this.accuracy){
 			console.log('The enemy ship hit you!');
@@ -20,14 +23,17 @@ class AlienShip{
 		}
 	}
 }
-
+//make the human ship object
 const ussAssembly = {
+	//assign it standard properties
 	hull: 20,
 	firepower: 5, 
 	accuracy: 0.7, 
+	//make a receive damage method which will reduce hull strengh by the firepower of the opposing ship
 	receiveDamage(damage){
 		this.hull -= damage;
 	},
+	//made an attack method which uses a random number to determine if the attack was succesful or not
 	attack(){
 		if(Math.random() < this.accuracy){
 			console.log('Successful attack! You hit the enemy ship!');
@@ -50,3 +56,24 @@ const enemyShip = new AlienShip;
 // for(let i = 0; i < 6; i++){
 // 	enemyShips[i].push((new AlienShip));
 // }
+
+
+const battle = (ship1, ship2) => {
+	ship2.receiveDamage(ship1.attack());
+	if (ship2.hull > 0) {
+		ship1.receiveDamage(ship2.attack());
+		if(ship1.hull <= 0) {
+			console.log("You're ship is defeated!");
+		}
+	} else {
+		console.log("You defeated the enemy ship!");
+	}
+	return
+}
+
+let count = 0;
+//make a loop to continue battling one ship until either's hull goes to 0
+while(enemyShip.hull > 0 && ussAssembly.hull > 0 && count < 7) {
+	battle(ussAssembly, enemyShip);
+	count++;
+}
