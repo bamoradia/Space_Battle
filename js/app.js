3a90cf2788902edf8152137adebc8096b58f4bec
Badal Moradia <badal@i360llc.com>
2018-06-20 12:11:10 -0500

    added alien ship class and human ship object

--- js/app.js
+++ js/app.js
@@ -7,6 +7,36 @@ class AlienShip{
 		this.firepower = randomNumber(2, 4)
 		this.accuracy = randomNumber(.6, .8, 'dec')
 	}
+	receiveDamage(damage){
+		this.hull -= damage;
+	}
+	attack(){
+		if(Math.random() < this.accuracy){
+			console.log('The enemy ship hit you!');
+			return this.firepower;
+		} else {
+			console.log("The enemy ship missed it's attack");
+			return 0
+		}
+	}
+}
+
+const ussAssembly = {
+	hull: 20,
+	firepower: 5, 
+	accuracy: 0.7, 
+	receiveDamage(damage){
+		this.hull -= damage;
+	},
+	attack(){
+		if(Math.random() < this.accuracy){
+			console.log('Successful attack! You hit the enemy ship!');
+			return this.firepower;
+		} else {
+			console.log('You missed the enemy ship!');
+			return 0
+		}
+	}
 }
 
 //make a random number generator that will return a value between two input values
@@ -15,6 +45,8 @@ const randomNumber = (lowNum, highNum) => {
 	return ((Math.random() * diff) + lowNum);
 }
 
+const enemyShip = new AlienShip;
 
-const enemyShip1 = new AlienShip;
-
+// for(let i = 0; i < 6; i++){
+// 	enemyShips[i].push((new AlienShip));
+// }
